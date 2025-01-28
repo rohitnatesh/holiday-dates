4c48b59980d057ed8c28cdeb0aac09be93a80f39
Rohit Natesh <rohit.natesh95@gmail.com>
2025-01-28 16:00:25 -0500

    Add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,42 @@
 import type { Metadata } from "next";
+import { Box, Typography } from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export const metadata: Metadata = {
-  title: "World Holidays and Events",
-  description: "",
+    title: "World Holidays and Events",
+    description: "",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    marginTop="3rem"
+                    marginBottom="2rem"
+                    component="header"
+                >
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        style={{ userSelect: "none" }}
+                    >
+                        World Holidays and Events
+                    </Typography>
+                </Box>
+                {children}
+            </body>
+        </html>
+    );
 }
