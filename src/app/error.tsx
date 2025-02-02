c0d0c3e5a26c34486f1beb564910e4592d5b4b8b
Rohit Natesh <rohit.natesh95@gmail.com>
2025-02-01 23:44:59 -0500

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect } from "react";
+import { AlertCircle } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    return (
+        <section>
+            <Alert variant="destructive" className="max-w-fit">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Oops! Something&apos;s Not Right</AlertTitle>
+                <AlertDescription className="pr-8">
+                    There was a problem with this section.
+                    <Button
+                        variant="link"
+                        className="px-2"
+                        onClick={() => reset()}
+                    >
+                        Try Again?
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        </section>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,15 @@ export default function RootLayout({
         <html lang="en">
             <body className="bg-slate-50 px-12 py-8">
                 <header>
-                    <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl mt-8">
+                    <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl mt-2">
                         World Holidays and Events
                     </h1>
+                    <p className="text-xl text-muted-foreground mt-2 mb-12">
+                        Explore holidays and events worldwide
+                    </p>
                 </header>
                 {children}
+                <footer></footer>
             </body>
         </html>
     );
